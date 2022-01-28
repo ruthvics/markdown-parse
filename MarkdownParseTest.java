416255af4803e5918d20a36de1acffd35efadf12
ruthvics <60528254+ruthvics@users.noreply.github.com>
2022-01-28 06:19:41 -0800

    updt

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,36 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+import java.nio.file.Path;
 
 public class MarkdownParseTest {
+
+    List<String> answ;
+    Path test1;
+
     @Test
     public void addition() {
-        assertEquals(2, 1 + 1);
+        assertEquals(2, 1 + 1); // checks if these two parameters are equal
+    }
+
+    @Test
+    public void parseTestPathOne() {
+        // MarkdownParse.getLisnks("test-file.md");
+        answ = List.of("https://something.com", "some-page.html");
+        // ArrayList<String>(MarkdownParse.getLinks("test-file.md"));
+        // ArrayList<Strings> = new ArrayList<Strings>()
+        // assertEquals(MarkdownParse.getLinks("test-file.md"), "[https://something.com,
+        // some-page.html]");
+
+        test1 = Path.of("test-file.md");
+
+        // List.of("a", "b", "c")
+    }
+
+    @Test
+    public void testContentOne() {
+        assertEquals(answ, MarkdownParse.getLinks(test1));
     }
 }
